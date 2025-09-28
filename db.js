b72363a43e2efb47bffbd509674c6809021ad635
Tu Nombre <tu@correo.com>
2025-09-28 16:56:56 -0600

    Detalle Final

--- db.js
+++ db.js
@@ -1,24 +1,8 @@
-/*import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306, 
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-   ssl: {rejectUnauthorized: true }
-});*/
-
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const useSSL = process.env.DB_SSL === 'true'; // activas TLS “relajado” con una env
+const useSSL = process.env.DB_SSL === 'true'; 
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
